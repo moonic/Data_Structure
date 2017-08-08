1f20352847306516de201742e19b50e523a9ec8d
moonic <1160708553@qq.com>
2017-08-08 23:03:54 +0800

    Create Selection.java

---
+++ algorithm/Selection.java
@@ -0,0 +1,15 @@
+public class Selection
+{
+  public static void sort(Comparable[] a)
+{ // 将a[]按升序排列
+    int N = a.length; // 数组长度
+    for (int i = 0; i < N; i++)
+  { // 将a[i]和a[i+1..N]中最小的元素交换
+    int min = i; // 最小元素的索引
+    for (int j = i+1; j < N; j++)
+      if (less(a[j], a[min])) min = j;
+    exch(a, i, min);
+      }
+    }
+// less()、exch()、isSorted()和main()方法见“排序算法类模板”
+}
