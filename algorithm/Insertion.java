518ab74210169950667675dde670744170642fc5
moonic <1160708553@qq.com>
2017-10-04 22:53:54 +0800

    Update Insertion.java

--- algorithm/Insertion.java
+++ algorithm/Insertion.java
@@ -5,7 +5,7 @@ public class Insertion
   int N = a.length;
   for (int i = 1; i < N; i++)
   { // 将 a[i] 插入到 a[i-1]、a[i-2]、a[i-3]...之中
-    for (int j = i; j > 0 && less(a[j], a[j-1]); j--)
+    for (int j = i; j > 0 && less(a[j], a[j-1]); j--) 
   exch(a, j, j-1);
      }
   }
