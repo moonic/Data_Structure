53310aa339ccba824007310d1cd5c558c24776f4
moonic <1160708553@qq.com>
2018-01-20 23:26:06 +0800

    Update BinarySearch.java

--- algorithm/BinarySearch.java
+++ algorithm/BinarySearch.java
@@ -4,7 +4,7 @@ public class BinarySearch{
        int lo = 0;
        int hi = a.length - 1;
    while (lo <= hi){ // 被查找的键要么不存在，要么必然存在于 a[lo..hi] 之中
-     int mid = lo + (hi - lo) / 2;
+     int mid = lo + (hi - lo) / 2; 
   if (key < a[mid]) hi = mid - 1;
     else if (key > a[mid]) lo = mid + 1;
   else return mid;
